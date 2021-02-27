9fa1e3697b7dc4d679b9587c9a76e829f4bf9204
Bryan Ly <bryan.ly@kp.org>
2021-02-27 09:33:01 -0800

    adding beta scripts

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,1 +1,33 @@
 # this file is used to call the jenkins install script
+
+resource "null_resource" "example1" {
+
+  # move bash script to Jenkins
+  provisioner "file" {
+    source      = "scripts/0.0.0.beta_test.sh"
+    destination = ""
+
+    connection {
+      type     = "ssh"
+      user     = "Administrator"
+      private_key = ""
+    }
+  }
+
+  # execute bash script to configure Jenkins
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x /tmp/*sh",
+  #     "sudo /tmp/system_setup.sh",
+  #     "sudo /tmp/install_docker.sh",
+  #     "sudo /tmp/install_kubernetes_packages.sh",
+  #     "sudo /tmp/kubeadm_init.sh",
+  #     "tail -n2 /tmp/kubeadm_init_output.txt | head -n 1",
+  #   ]
+  #   connection {
+  #     type        = ""
+  #     user        = ""
+  #     private_key = ""
+  #   }
+  # }
+}
